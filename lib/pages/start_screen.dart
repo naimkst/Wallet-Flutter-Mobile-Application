6c5cb02ff109507344cea71496daf0a6543f56ce
Naim <naimkst@gmail.com>
2022-12-13 21:20:37 +0600

    Upload all

--- lib/pages/start_screen.dart
+++ lib/pages/start_screen.dart
@@ -37,7 +37,6 @@ class _StartPageState extends State<StartPage> {
                   ),),
                   Row(
                     children: [
-
                     ],
                   ),
                 ],
